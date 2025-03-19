bd8ae0e0af1f24b96b93ae4926dcbc53d74f0f46
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-19 11:18:43 +0300

    updated index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function (a,b) {
+    return(a/b);
+}
+console.log(divide(2000,100));
+
+const square = (par) => par * par ;
+console.log(square(8));
+
+const add = (x, y) => x + y;
+console.log(add(6,7));
